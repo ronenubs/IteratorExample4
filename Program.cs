fd5410152c8d135e36265cd1877494762e4f7ecc
Ron Enubs <ronenubs@gmail.com>
2020-11-04 11:48:40 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zoo theZoo = new Zoo();
+            
+            theZoo.AddMammal("Whale");
+            theZoo.AddMammal("Carabao");
+
+            theZoo.AddBird("Penguin");
+            theZoo.AddBird("Eagle");
+
+            //foreach(string name in theZoo)
+            //{
+            //    Console.Write(name + " ");
+            //}
+
+            //Console.WriteLine();
+
+            foreach(string name in theZoo.Birds)
+            {
+                Console.Write(name + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zoo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorExample4
+{
+    public class Zoo : IEnumerable
+    {
+        private List<Animal> animals = new List<Animal>();
+
+        public void AddMammal(string name)
+        {
+            animals.Add(
+                new Animal
+                {
+                    Name = name,
+                    Type = Animal.TypeEnum.Mammal
+                }
+            );
+        }
+
+        public void AddBird(string name)
+        {
+            animals.Add(
+                new Animal
+                {
+                    Name = name,
+                    Type = Animal.TypeEnum.Bird
+                }
+            );
+        }
+
+        public IEnumerable Mammals
+        {
+            get { return AnimalsForType(Animal.TypeEnum.Mammal); }
+        }
+
+        public IEnumerable Birds
+
+        {
+            get { return AnimalsForType(Animal.TypeEnum.Bird); }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Animal theAnimal in animals)
+            {
+                yield return theAnimal.Name;
+            }
+        }
+
+        private IEnumerable AnimalsForType(Animal.TypeEnum type)
+        {
+            foreach (Animal theAnimal in animals)
+            {
+                if (theAnimal.Type == type)
+                {
+                    yield return theAnimal.Name;
+                }
+            }
+        }
+
+        private class Animal
+        {
+            public enum TypeEnum { Bird, Mammal }
+
+            public string Name { get; set; }
+            public TypeEnum Type { get; set; }
+        }
+    }
+}
